7ceb6b1acadb381d2dac583fafa86c7f5f4a1c56
rainbowatcher <rainbow-w@qq.com>
2023-02-17 21:44:20 +0800

    feat: add isAsyncFunction

--- test/is.test.ts
+++ test/is.test.ts
@@ -1,9 +1,17 @@
 import { describe, expect, it } from "vitest"
-import { isUndefined } from "../src"
+import { isAsyncFunction, isUndefined } from "../src"
 
 describe("Is Utils", () => {
   const ud = undefined
   it("isUndefined", () => {
     expect(isUndefined(ud)).toBe(true)
+  })
+
+  it("isAsyncFunction", () => {
+    const func0 = () => {}
+    const asyncFunc0 = async () => {}
+    expect(isAsyncFunction(ud)).toBe(false)
+    expect(isAsyncFunction(func0)).toBe(false)
+    expect(isAsyncFunction(asyncFunc0)).toBe(true)
   })
 })
