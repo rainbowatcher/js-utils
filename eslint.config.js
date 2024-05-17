db240513bfd085cd2b380319e6f79312b57be442
rainbowatcher <rainbow-w@qq.com>
2024-05-17 10:38:18 +0800

    feat: upgrade to eslint v9

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "@rainbowatcher/eslint-config";
+
+export default defineConfig({
+    gitignore: true,
+    typescript: true,
+    style: true,
+    json: true,
+    markdown: true,
+    yaml: true,
+});
