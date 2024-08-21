14cab41510076b88d4abab4b149d629831781e71
rainbowatcher <rainbow-w@qq.com>
2024-08-21 21:13:02 +0800

    feat: add type KeysOf

--- packages/maybe/src/index.ts
+++ packages/maybe/src/index.ts
@@ -13,8 +13,20 @@ export type MaybeString<T> = string | T
 
 export type MaybeNumber<T> = number | T
 
-
 export type MaybeObject<T> = Record<string, unknown> | T
 
-
 export type MaybePromise<T> = Promise<T> | T
+
+/**
+ * @example
+ * ```ts
+ * type A = {
+ *     name: string
+ *     age: number
+ * }
+ *
+ * type B = KeysOf<A> // "name"
+ * type C = KeysOf<A, number> // "age"
+ * ```
+ */
+export type KeysOf<T, KeyType = string> = keyof { [K in keyof T as T[K] extends KeyType ? K : never]: T[K] }
